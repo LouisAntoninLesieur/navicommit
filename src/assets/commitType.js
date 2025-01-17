63cc60e93952b992c27a965a7d50ebd5fc243b88
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2025-01-17 16:29:56 +0100

    feat: commit types and descriptions

---
+++ src/assets/commitType.js
@@ -0,0 +1,30 @@
+export const commitType = [
+  {
+    value: 'feat',
+    description: '- Add a new feature',
+  },
+  {
+    value: 'fix',
+    description: '- Fix a bug',
+  },
+  {
+    value: 'refactor',
+    description: '- Improve code structure without changing behavior',
+  },
+  {
+    value: 'style',
+    description: '- Code style changes (formatting, spaces, etc.)',
+  },
+  {
+    value: 'test',
+    description: '- Add or fix tests',
+  },
+  {
+    value: 'docs',
+    description: '- Documentation changes only',
+  },
+  {
+    value: 'chore',
+    description: '- Other changes (e.g., update .gitignore)',
+  }
+];
